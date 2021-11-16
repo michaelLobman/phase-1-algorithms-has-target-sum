63ff88d57a190864cbc82634e49acb0774ef06e6
Michael Lobman <michael.lobman@gmail.com>
2021-11-16 16:46:56 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let x = i + 1; x < array.length; x++) {
+      if(array[i] + array[x] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+
+O(n^2)
+
+Given an input of n elements, the worst case scenario is that 
+the algorithm needs to make O(log n) iterations.
+
+Given an input of n elements, the algorithm will retain a constant
+space optimization.
 */
 
-/* 
-  Add your pseudocode here
+/*  
+
+  build function with parameter of array and target
+    iterate through the array with two variables
+    an i and a j
+    one inside the other
+   checking to see if i and j together = the target
+
+   test case [1, 2, 3], 9,
+   test case [1, 2, 3] 4
+
 */
 
 /*
-  Add written explanation of your solution here
+
+hasTargetSum iterates through the array parameter twice.
+the first iteration is through the i variable, the second
+is by way of the x variable. 
+
+the function checks the sum of these two numbers against the 
+target parameter. if, at any point, it finds that two numbers
+in the array add up to the target, it exits the iteration loop
+and returns true
+
+once it checks every possible sum against the target without
+returning true, the function returns false
+*/
+
+/*
+Write a function with two inputs/parameters: an array and a target number.
+the function should iterate through the array, looking to see if any pair
+of numbers in the array add up to the target number
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,12 +60,14 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  console.log("Expected: true");
+  console.log("=>", hasTargetSum([1, 2, 3], 4));
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1,2,3], 7));
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
